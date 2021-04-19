7139f77edb63010459fb5c2331a5fa562ee1738b
abauruel <abauruel@gmail.com>
2021-04-19 19:41:05 -0300

    feat: :sparkles: add feature from first day nlw5

---
+++ src/components/Button.tsx
@@ -0,0 +1,38 @@
+import colors from "../styles/colors"
+import { Text, TouchableOpacity, StyleSheet, TouchableOpacityProps } from 'react-native'
+import React from 'react'
+
+interface ButtonProps extends TouchableOpacityProps {
+    title: string
+}
+export function Button({ title, ...rest }: ButtonProps) {
+    return (
+        <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.7}
+            {...rest}
+        >
+            <Text style={styles.buttontext}>{title} </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+
+
+    },
+    buttontext: {
+        color: colors.white,
+        fontSize: 24
+    }
+
+})
